ba5c0801dc7b2bc5fd401c092d49190f1e56c256
Abdelkhalek7 <100151191+Abdelkhalek7@users.noreply.github.com>
2022-09-14 13:58:22 +0200

    cors

--- src/main/java/com/example/springjwt/security/securtyconfig.java
+++ src/main/java/com/example/springjwt/security/securtyconfig.java
@@ -24,7 +24,13 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -34,50 +40,6 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class securtyconfig{
 
-/*    AuthenticationManager authenticationManager;
-
-    @Autowired
-    UserDetailsService userDetailsService;
-
-   private BCryptPasswordEncoder bCryptPasswordEncoder;
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-       auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeHttpRequests().anyRequest().permitAll();
-        http.addFilter(new CustemAuthrizaionFilter(authenticationManagerBean()));
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean()throws Exception{
-        return super.authenticationManagerBean();
-    }*/
-
-  /*  @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(userDetailsService);
-        authenticationManager = authenticationManagerBuilder.build();
-        http.authorizeHttpRequests().anyRequest().permitAll();
-        http.csrf().disable();
-
-
-                http.authenticationManager(authenticationManager)
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        return http.build();
-
-
-
-    }*/
 
 
 
@@ -88,7 +50,10 @@ public class securtyconfig{
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         AbstractAuthenticationProcessingFilter filter = new CustemAuthrizaionFilter(authenticationManager());
         filter.setFilterProcessesUrl("/api/login");
-        //http.csrf().disable();
+        http
+                // by default uses a Bean by the name of corsConfigurationSource
+                .cors(withDefaults());
+        http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/**").permitAll();
         http.authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/**").permitAll();
@@ -111,7 +76,15 @@ public class securtyconfig{
     AuthenticationManager authenticationManager() throws Exception {
         return configuration.getAuthenticationManager();
     }
-
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Autowired
     void configure(AuthenticationManagerBuilder builder) throws Exception {
         builder.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
